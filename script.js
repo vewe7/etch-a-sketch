e8a933c558743d3e31e4985dfb6ced561f703466
vewe7 <naomi.yoon.do@gmail.com>
2023-06-25 15:47:08 -0500

    Add a button and function to change grid size

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-let size = 16;
-
+const initSize = 16;
 const grid = document.querySelector('.grid');
 
-for (let i = 0; i < size; i++)
-{
-    let col = document.createElement('div');
-    col.classList.add('column');
-    grid.appendChild(col);
+function generateGrid(size) {
+    for (let i = 0; i < size; i++) {
+        let col = document.createElement('div');
+        col.classList.add('column');
+        grid.appendChild(col);
 
-    for (let j = 0; j < size; j++)
-    {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        square.addEventListener('mouseover', () =>  {
-            square.classList.add('black');
-        });
-        col.appendChild(square);
+        for (let j = 0; j < size; j++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            col.appendChild(square);
+            square.addEventListener('mouseover', () =>  {
+                square.classList.add('black');
+            });
+        }
     }
 }
+
+function destroyGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+function resizeInput() {
+    let input = prompt("Enter a new grid width (max: 100).");
+    input = parseInt(input);
+
+    if (input >= 1 && input <= 101)
+    {
+        destroyGrid();
+        generateGrid(input);
+    }
+}
+
+generateGrid(initSize);
+
